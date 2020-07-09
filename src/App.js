a29f03543ca33ab05480a6bd005db893cf936026
checIt-lab <57136184+checIt-lab@users.noreply.github.com>
2020-07-09 11:45:33 -0400

    primer comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {makeStyles, Grid, Paper, Container, CssBaseline} from '@material-ui/core';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+       // marginTop: theme.spacing(4),
+      },
+      paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        fontWeight:'bold',
+        color:'#080808',
+      },
+      barraOne:{
+
+      }
+}));
+
+
 
 function App() {
+    
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container component="main" maxWidth="md">
+        <CssBaseline />
+        <div className={classes.root}>
+             <Grid container spacing={5}>
+                <Grid item xs={12}>
+                    <div className={classes.paper}>
+                        <div className ="animate__animated animate__bounce animate__repeat-2">
+                          <img className={classes.logo} src={require('./assets/logo.png')} alt='logo'/>
+                        </div>
+                    </div>
+                </Grid>
+                <Grid item xs={6}>
+                    <div className={classes.paper}>
+                         <hr  width='40%'  align="left" size='5' color="#FF0000"/>
+                        MATEIAL DE CONSTRUCION
+                    </div>
+                </Grid>
+                <Grid item xs={6}>
+                    <div className={classes.paper}>
+                         <hr  width='40%'  align="right" size='5'color="#007AC0"/>
+                        DIVISION TABLEROS DE MATERA
+                    </div>
+                </Grid>
+                <Grid item xs={12}>
+                    <div className={classes.paper}>
+                    <hr  width='20%'  align="center" size='5'color="#0D0D0D"/>
+                        ACERO GALVANIZADO
+                    </div>
+                </Grid>
+             </Grid>
+         </div>
+    </Container>
+         
   );
 }
 
